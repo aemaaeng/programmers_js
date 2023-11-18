4bd4d90070ed613039c1a24aac9bc50947ff877a
aemaaeng <haemin120@naver.com>
2023-11-18 09:40:17 +0900

    :memo: 카드 뭉치

---
+++ Lv 1/Cards.js
@@ -0,0 +1,16 @@
+// 카드 뭉치 - https://school.programmers.co.kr/learn/courses/30/lessons/159994
+function solution(cards1, cards2, goal) {
+  let answer = "Yes";
+
+  for (const word of goal) {
+    if (cards1[0] === word) {
+      cards1.shift();
+    } else if (cards2[0] === word) {
+      cards2.shift();
+    } else {
+      return "No";
+    }
+  }
+
+  return answer;
+}
